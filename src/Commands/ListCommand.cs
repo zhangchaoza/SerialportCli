69dcff8f33a6890d5c0dda4f1b5c7f91138d737e
zhangchao <zackability@hotmial.com>
2020-12-03 14:28:15 +0800

    fix(list): error when no ports

--- src/Commands/ListCommand.cs
+++ src/Commands/ListCommand.cs
@@ -25,10 +25,13 @@ namespace SerialportCli
             var pages = GetPagesCount(ports.Length);
 
             var pagesPad = pages.ToString().Length + 1;
-            var namePad = ports.Max(i => i.Length) + 1;
-            for (int i = 0; i < pages; i++)
+            if (ports.Any())
             {
-                Console.WriteLine("{0} {1}", i.ToString().PadLeft(pagesPad).Pastel(Color.Gray), string.Join(" ", ports.Skip(PAGE_SIZE * i).Take(PAGE_SIZE).Select(i => $"{i.PadRight(namePad).Pastel(Color.LightGreen)}")));
+                var namePad = ports.Max(i => i.Length) + 1;
+                for (int i = 0; i < pages; i++)
+                {
+                    Console.WriteLine("{0} {1}", i.ToString().PadLeft(pagesPad).Pastel(Color.Gray), string.Join(" ", ports.Skip(PAGE_SIZE * i).Take(PAGE_SIZE).Select(i => $"{i.PadRight(namePad).Pastel(Color.LightGreen)}")));
+                }
             }
             Console.WriteLine($"{"Total:".Pastel(Color.Gray)} {ports.Length.ToString().Pastel(Color.DarkRed)}");
 
